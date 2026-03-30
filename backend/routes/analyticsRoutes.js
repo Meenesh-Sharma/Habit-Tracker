e8bd003f8a2ff12242aca1ba70b478231386cfa2
Meenesh-coder <meenesh15102002@gmail.com>
2026-03-30 22:54:04 +0530

    Updated analyticsRoutes.js: added /test route

--- backend/routes/analyticsRoutes.js
+++ backend/routes/analyticsRoutes.js
@@ -1,12 +1,4 @@
-// import express from "express"
-// import { getAnalytics } from "../controllers/analyticsController.js"
-// import { authMiddleware } from "../middleware/authMiddleware.js"
 
-// const router = express.Router()
-
-// router.get("/",authMiddleware,getAnalytics)
-
-// export default router 
 
 import express from "express"
 import { getAnalytics } from "../controllers/analyticsController.js"
